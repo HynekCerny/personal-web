776df5320e51b1eaab6569a87ff9f5dd24586dad
Hynek Cerny <you@example.com>
2025-04-23 21:21:14 +0200

    Add GTM view_experience_cl event

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,21 @@
 import { FaLinkedin } from "react-icons/fa6";
 
+declare global {
+    interface Window {
+      dataLayer: Record<string, any>[];
+    }
+  }  
+
 const Hero = () => {
+    const handleClick = () => {
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            event: 'view_experience_click',
+            buttonText: 'View Experience'
+        });
+        document.getElementById('experiences')?.scrollIntoView({ behavior: 'smooth' });
+    };
+
     return (
         <section className="w-full flex flex-col items-center sm:items-start text-center sm:text-left mt-10 pt-24 md:pt-24 lg:pt-24 pb-20 bg-gradient-to-tr from-blue-100 to-white">
             <div className="container md:px-10 max-w-screen-xl mx-auto px-4 w-full">
@@ -14,9 +29,7 @@ const Hero = () => {
                         </p>
                     </div>
                     <div className="flex justify-center md:justify-end space-x-4 mt-4 md:mt-0">
-                        <button onClick={() => {
-                            document.getElementById('experiences')?.scrollIntoView({ behavior: 'smooth' });
-                        }}
+                        <button onClick={() => { handleClick }}
                             className="cursor-pointer px-4 lg:px-8 py-3 text-base lg:text-xl bg-white text-blue-700 border-2 border-blue-700 rounded-lg hover:bg-blue-100 transition-colors flex items-center space-x-2 mt-4 md:mt-0 justify-center">
                             <span>View Experience</span>
                         </button>
