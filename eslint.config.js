eca6f3b63e0e14ce2937bbda8c6dbdd5fb3c47a9
Hynek Cerny <HynekCerny@users.noreply.github.com>
2026-03-26 23:08:43 +0100

    fix: migrate eslint config to flat config, fix lint script

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,19 @@
-import nextPlugin from 'eslint-config-next';
+import js from '@eslint/js';
+import nextPlugin from '@next/eslint-plugin-next';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
 
 export default [
-  nextPlugin,
-]
+  { ignores: ['.next/**', 'out/**', 'node_modules/**'] },
+  js.configs.recommended,
+  {
+    plugins: {
+      '@next/next': nextPlugin,
+      'react-hooks': reactHooksPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+    },
+  },
+];
